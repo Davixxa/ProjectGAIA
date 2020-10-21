934dd6905c5f6f3b9f4956b7d75bfbacb5c4bb61
B-a-n-g <73177037+B-a-n-g@users.noreply.github.com>
2020-10-21 10:45:43 +0200

    Update RunSimulation.java

--- source/RunSimulation.java
+++ source/RunSimulation.java
@@ -3,6 +3,14 @@
 public class RunSimulation {
 
 	public static void main(String[] args){
+		//welcomeMessage();
+		//parameter
+		Colony[] homes = new Colony[1];
+		Graph graph = new Graph(2,2,homes,0.50,5);
+		Ant[] ants = new Ant[1](homes[0]);
+		Simulator simCity = new Simulator(graph,ants,1,5,);
+		Node node = new Node(5);
 		
+		Visualizer visualizer = new Visualizer(graph,true,node,ants);
 	}
 }
