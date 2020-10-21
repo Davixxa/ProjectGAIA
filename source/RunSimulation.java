01abdcdad553fb854c1b77391aacae601fce5452
B-a-n-g <73177037+B-a-n-g@users.noreply.github.com>
2020-10-21 11:24:10 +0200

    Runsimulation struktur

--- source/RunSimulation.java
+++ source/RunSimulation.java
@@ -3,14 +3,29 @@
 public class RunSimulation {
 
 	public static void main(String[] args){
-		//welcomeMessage();
-		//parameter
-		Colony[] homes = new Colony[1];
-		Graph graph = new Graph(2,2,homes,0.50,5);
-		Ant[] ants = new Ant[1](homes[0]);
-		Simulator simCity = new Simulator(graph,ants,1,5,);
-		Node node = new Node(5);
+		welcomeMessage();
 		
-		Visualizer visualizer = new Visualizer(graph,true,node,ants);
+		//Node creation
+		
+		//Ant creation
+		
+		//Colony creation
+		
+		//Graph creation
+		
+		//Additional parameters
+		
+		//Running the simulation
+		
 	}
+	
+	/*
+	* This method prints the welcome screen when the program is opened
+	*/
+	private static void welcomeMessage(){
+		
+	}
+	
+	
+	
 }
---
+++ source/eksempel.java
@@ -0,0 +1,14 @@
+public class eksempel{
+	public static main(String[] args){
+		Colony[] homes = new Colony[1];
+		homes[0] = new Colony();
+		Graph graph = new Graph(2,2,homes,0.50,5);
+		Ant[] ants = new Ant[1];
+		ants[0] = new Ant(homes[0]);
+		Simulator simCity = new Simulator(graph,ants,1,5);
+		Node node = new Node(5);
+		
+		Visualizer visualizer = new Visualizer(graph,false,node,ants);
+		visualizer.printStatus();
+	}
+}
