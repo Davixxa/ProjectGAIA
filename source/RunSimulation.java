957467b8b910ff5f2063c0e8134190afaf9dc9d4
lynetma2 <99sund@gmail.com>
2020-10-23 00:04:40 +0200

    Update RunSimulation.java
    indlende arbejde, alle inputs bliver nu skaffet...

--- source/RunSimulation.java
+++ source/RunSimulation.java
@@ -9,7 +9,50 @@ public class RunSimulation {
 		
 		System.out.println("Please enter the probability that a node will start as a node containing sugar. (Between 0 and 1.0)");
 		double sugarProb = getDouble(0,1);
-		 
+		
+		System.out.println("Please enter the average amount of sugar in a node. (Greater than 0)");
+		int averageSugar = getInt(0);
+		
+		System.out.println("Please enter the number of units of sugar that an ant can carry. (Greater than 0)");
+		int carriedSugar = getInt(0);
+		
+		System.out.println("Please enter the number of units of pheromones dropped by an ant when passing through a node. (Greater than 0)");
+		int droppedPheromones = getInt(0);
+		
+		System.out.println("Please enter the total number of ant colonies. (Stricly greater than 0)");
+		int totalColonies = getInt(1);
+		
+		System.out.println("Please enter the number of ants initially at each colony. (Greater than 0)");
+		int initialAnts = getInt(0);
+		
+		System.out.println("Please enter the type of graph to use: \n 1. grid \n 2. file");
+		boolean isGrid = (1 == getIntBetween(1,2));
+		
+		//Checks which additional information is needed.
+		if(isGrid){
+			System.out.println("Please enter the width of the graph. (Greater than 3)");
+			int width = getInt(3);
+			
+			System.out.println("Please enter the depth of the graph. (Greater than 3)");
+			int depth = getInt(3);
+		} 
+		else {
+			System.out.println("Please enter the name of the file storing the graph.");
+			String filename = scanner.next();
+		}
+		
+		System.out.println("Please enter the total simulation time. (Greater than 0)");
+		int totalSimulationTime = getInt(0);
+		
+		System.out.println("Please enter the desired viewing mode:  \n 1. textual summary \n 2. graphical representation");
+		boolean isGraph = (2 == getIntBetween(1,2));
+		
+		//Checks if additional information is needed.
+		if(!isGraph){
+			System.out.println("Please enter the update frequency. (Stricly greater than 0)");
+			int updateFrequency = getInt(1);
+		}
+		
 		//File or grid
 		/*Colony
 			Total amount of colonies
@@ -72,6 +115,36 @@ public class RunSimulation {
 				System.out.println("Please write a number between " + min + " and " + max + ".");
 			}
 		} while(number > max || number < min);
+		
+		return number;
+	}
+	
+	private static int getInt(int min){
+		int number;
+		
+		do {
+			Scanner sc = new Scanner(System.in);
+			number = sc.nextInt();
+		
+			if(number < min){
+				System.out.println("Please write a number greater than " + min + ".");
+			}
+		} while(number < min);
+		
+		return number;
+	}
+	
+	private static int getIntBetween(int min, int max){
+		int number;
+		
+		do {
+			Scanner sc = new Scanner(System.in);
+			number = sc.nextInt();
+		
+			if(number < min || number > max){
+				System.out.println("Please write a number greater than " + min + ".");
+			}
+		} while(number < min || number > max);
 		
 		return number;
 	}
