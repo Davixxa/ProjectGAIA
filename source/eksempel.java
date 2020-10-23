4552bbc91b8f5a905d8175fa52aa10b1c95c449c
lynetma2 <99sund@gmail.com>
2020-10-23 16:56:46 +0200

    Opdateret eksemplet til at virke når man vælger en fil som input
    Der skal være en sammenhæng mellem antallet af colonier i filen og det som grafen forsøger at vise. Dermed er colony[] blevet ændret til at have 2 elementer da det er hvad filen beskriver.

--- source/eksempel.java
+++ source/eksempel.java
@@ -5,19 +5,19 @@ public class eksempel{
 	private static Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args){
-		Colony[] homes = new Colony[5];
+		Colony[] homes = new Colony[2];
 		for(int i=0; i<homes.length;i++){
 			homes[i] = new Colony();
 		}
-		Graph graph = new Graph(16,16,homes,0.50,5);
-		//Graph graph = new Graph("graph1",homes,0.50,5);
+		//Graph graph = new Graph(16,16,homes,0.50,5);
+		Graph graph = new Graph("graph1",homes,0.50,5);
 		Ant[] ants = new Ant[20];
 		for(int i = 0; i < ants.length; i++){
-			ants[i] = new Ant(homes[i % 5]);
+			ants[i] = new Ant(homes[i % 2]);
 		}
 		Simulator simCity = new Simulator(graph,ants,20,1);
 		
-		Visualizer visualizer = new Visualizer(graph,true,homes[0],ants);
+		Visualizer visualizer = new Visualizer(graph,false,homes[0],ants);
 		visualizer.display();
 		
 		//do while loop så simuleringen kan køres i steps
