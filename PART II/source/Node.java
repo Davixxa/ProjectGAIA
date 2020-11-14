62b96d68c78c04fa965e26fc2a9da30defd00601
B-a-n-g <73177037+B-a-n-g@users.noreply.github.com>
2020-11-14 22:36:38 +0100

    Update Node.java

--- PART II/source/Node.java
+++ PART II/source/Node.java
@@ -3,14 +3,14 @@ public class Node{
     private int sugarLevel;
 
     /*
-    * Constructor taking no arguments, initialising the node with a the default sugar level of 0.
+    *Constructor taking no arguments, initialising the node with a the default sugar level of 0.
 	*/
     public Node() {
         this.sugarLevel = 0;
     }
 
     /*
-    * Second Constructor taking one argument, initialising the node with the sugar level provided in the variable sugar.
+    *Second Constructor taking one argument, initialising the node with the sugar level provided in the variable sugar.
     */
     public Node(int sugar) {
         this.sugarLevel = sugar;
@@ -27,7 +27,7 @@ public class Node{
 
 
     /*
-    * Decreases the amount of sugar in the current node by one unit unless there's no sugar in the current node
+    *Decreases the amount of sugar in the current node by one unit unless there's no sugar in the current node
     */
     public void decreaseSugar() {
 
@@ -37,7 +37,7 @@ public class Node{
 
 
     /*
-    * Sets the value of sugar in the node to a given value sugar.
+    *Sets the value of sugar in the node to a given value sugar.
     */
     public void setSugar(int sugar) {
         this.sugarLevel = sugar;
