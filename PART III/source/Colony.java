bb31ffc355f2b76f6833c8da774dc2955c889709
B-a-n-g <73177037+B-a-n-g@users.noreply.github.com>
2020-11-30 17:40:12 +0100

    setup

---
+++ PART III/source/Colony.java
@@ -0,0 +1,26 @@
+public class Colony extends Node{
+	//Instance varibles
+	
+	//Constructors
+	
+	public Colony(){
+		
+	}
+	
+	//Contract methods
+	
+	public void topUp(int sugar){
+		
+	}
+	
+	public void consume(){
+		
+	}
+	
+	public boolean hasStock(){
+		
+	}
+	
+	//May need to overwrite methods from class Node
+	
+}
---
+++ PART III/source/Graph.java
@@ -0,0 +1,40 @@
+public class Graph{
+	//Instance variables
+	
+	//Class attributes
+	
+	//Constructors
+	
+	/*
+	* Constructor for a grid graph
+	*/
+	public Graph(int width, int depth, Colony[] homes, double sugarProbability, int avgSugar){
+		
+	}
+	
+	/*
+	* Constructor for reading from a file
+	*/
+	public Graph(String Filename, Colony[] homes, double sugarProbability, int avgSugar){
+		
+	}
+	
+	//Contract methods
+	
+	public int pheromoneLevel(Node source, Node target){
+		
+	}
+	
+	public void raisePheromones(Node source, Node target, int amount){
+		
+	}
+	
+	public Node[] adjacentTo(Node node){
+		
+	}
+	
+	public void tick(){
+		
+	}
+	
+}
