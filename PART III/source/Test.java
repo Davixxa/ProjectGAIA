03d8655684de4a27c147c8c08957b878c95c0773
B-a-n-g <73177037+B-a-n-g@users.noreply.github.com>
2020-12-08 11:35:35 +0100

    fgjh

--- PART III/source/Test.java
+++ PART III/source/Test.java
@@ -5,7 +5,7 @@ public class Test{
 	public static void main(String[] args){
 		Colony[] homes = new Colony[2];
 		
-		Graph graph = new Graph(5,5,0.5,2);
-		graph.adjacantTo(homes[0]);
+		Graph graph = new Graph(5,5,homes,0.5,2);
+		graph.adjacentTo(homes[0]);
 	}
 }
