457f3047bc0bc4f6bc4c7e447c6ab4b5e86a170a
lynetma2 <99sund@gmail.com>
2020-12-14 15:32:50 +0100

    ryddet op og kommentarer -mangler lidt preconditions

---
+++ PART I/source/RunSimulation.java
@@ -0,0 +1,172 @@
+import java.util.Scanner;
+
+public class RunSimulation {
+	
+	private static Scanner scanner = new Scanner(System.in);
+	private static Visualizer visualizer;//For simplicity
+	private static Simulator simulator;//For simplicity
+	
+	public static void main(String[] args){
+		welcomeMessage();
+		boolean endProgram = false;
+		
+		do{
+			init();
+			
+			System.out.println("Please enter the total simulation time. (Greater than 0)");
+			int totalSimulationTime = getInt(0);
+			
+			System.out.println("Please enter the desired viewing mode:  \n 1. Textual summary \n 2. Graphical representation");
+			boolean isGraph = (2 == getIntBetween(1,2));
+			
+			//Checks if additional information is needed.
+			if(!isGraph){
+				System.out.println("Please enter the update frequency. (Stricly greater than 0)");
+				int updateFrequency = getInt(1);
+				
+				//Loop to run the simulation in textual summary
+				for(int simulationTime = 0; simulationTime < totalSimulationTime; simulationTime++){
+					simulator.tick();
+					if(simulationTime % updateFrequency == 0 || (simulationTime + 1) == totalSimulationTime)
+						visualizer.printStatus();
+				}
+			}
+			else {
+				visualizer.display();
+			
+				//Loop to run the simulation with grafical interface
+				for(int simulationTime = 0; simulationTime < totalSimulationTime; simulationTime++){
+					simulator.tick();
+					visualizer.update();
+				}
+			}
+
+			System.out.println("What would you like to do now? \n 1. Make another simulation \n 2. Stop the program");
+			endProgram = (2 == getIntBetween(1,2));
+			
+		}while(!endProgram);	
+		
+		System.exit(0);
+	}
+	
+	/*
+	* This method prints the welcome screen when the program is opened
+	*/
+	private static void welcomeMessage(){
+		System.out.println("Hello!\nThis program is a simplistic simulation of one or more ant colonies.\nThe nodes act as locations the ants can visit and they can contain sugar. The ants need sugar to survive.\nIf you plan to read the graph from a file instead of randomly generating it please make sure what you write matches the file.\n");
+	}
+	
+	/*
+	* Initializes the simulator and the visualizer
+	*/
+	private static void init(){
+		//First we start with inputs that are simple datatypes
+		System.out.println("Please enter the probability that a node will start as a node containing sugar. (Between 0 and 1.0)");
+		double sugarProb = getDoubleBetween(0,1.0);
+		
+		System.out.println("Please enter the average amount of sugar in a node. (Greater than 0)");
+		int averageSugar = getInt(0);
+		
+		System.out.println("Please enter the number of units of sugar that an ant can carry. (Greater than 0)");
+		int carriedSugar = getInt(0);
+		
+		System.out.println("Please enter the number of units of pheromones dropped by an ant when passing through a node. (Greater than 0)");
+		int droppedPheromones = getInt(0);
+		
+		
+		System.out.println("Please enter the total number of ant colonies. (Stricly greater than 0)");
+		int totalColonies = getInt(1);
+		Colony[] homes = new Colony[totalColonies];
+		
+		for(int i = 0; i < homes.length; i++){ //Initializes the Colonies in homes[]
+			homes[i] = new Colony();
+		}
+		
+		System.out.println("Please enter the number of ants initially at each colony. (Greater than 0)");
+		int initialAnts = getInt(0);
+		int totalAnts = initialAnts * totalColonies;
+		Ant[] ants = new Ant[totalAnts];
+		
+		for(int i = 0; i < ants.length; i++){ //Initializes the ants in ants[]
+			ants[i] = new Ant(homes[i % totalColonies]);
+		}
+		
+		System.out.println("Please enter the type of graph to use: \n 1. grid \n 2. file");
+		boolean isGrid = (1 == getIntBetween(1,2));
+		Graph graph;
+		
+		//Checks which additional information is needed.
+		if(isGrid){
+			System.out.println("Please enter the width of the graph. (Greater than 3)");
+			int width = getInt(3);
+			
+			System.out.println("Please enter the depth of the graph. (Greater than 3)");
+			int depth = getInt(3);
+			
+			graph = new Graph(width, depth, homes, sugarProb, averageSugar);
+		} 	
+		else {
+			System.out.println("Please enter the name of the file storing the graph.");
+			String filename = scanner.next();
+			
+			graph = new Graph(filename, homes, sugarProb, averageSugar);
+		}
+		
+		simulator = new Simulator(graph, ants, carriedSugar, droppedPheromones);
+		visualizer = new Visualizer(graph, isGrid, homes[0], ants);
+	}
+	
+	/*
+	* Returns an double from the user between min and max.
+	* Precondition: min < max.
+	*/
+	private static double getDoubleBetween(double min,double max){
+		double number;
+		
+		do {//Used for making sure the number is between min and max
+			number = scanner.nextDouble();
+		
+			if(number > max || number < min){
+				System.out.println("Please write a number between " + min + " and " + max + ".");
+			}
+		} while(number > max || number < min);
+		
+		return number;
+	}
+	
+	/*
+	* Returns an int from the user between min and max.
+	* Precondition: min < max.
+	*/
+	private static int getIntBetween(int min, int max){
+		int number;
+		
+		do {//Used for making sure the number is between min and max
+			number = scanner.nextInt();
+		
+			if(number < min || number > max){
+				System.out.println("Please write a number between " + min + " and " + max + ".");
+			}
+		} while(number < min || number > max);
+		
+		return number;
+	}
+	
+	/*
+	* Returns an int from the user greater than or equal to min.
+	*/
+	private static int getInt(int min){
+		int number;
+		
+		do {//Making sure the number is greater than min
+			number = scanner.nextInt();
+		
+			if(number < min){
+				System.out.println("Please write a number greater than " + min + ".");
+			}
+		} while(number < min);
+		
+		return number;
+	}
+	
+}
--- PART III/source/Graph.java
+++ PART III/source/Graph.java
@@ -15,23 +15,26 @@ public class Graph{
 	
 	/*
 	* Constructor for a grid graph
+	* PRECONDITION: width and depth > 2
 	*/
 	public Graph(int width, int depth, Colony[] homes, double sugarProbability, int avgSugar){
 		this.sugarProbability = sugarProbability;
 		this.avgSugar = avgSugar;
 		this.nodes = new Node[width*depth];
 		
-		//initializing nodes
+		//initializing colonies
 		for(int i = 0; i < homes.length; i++){
 			int place;
 			do{
+				//Finds a random place for the colony
 				place = RandomUtils.randomInt(nodes.length);
 			}while(nodes[place] != null);
 			nodes[place] = homes[i];
 		}
 		
-		
+		//initializing nodes
 		for(int i = 0; i < nodes.length; i++){
+			//initializing the places that are not a Colony
 			if(nodes[i] == null){
 				//Initalizes the nodes and decides if there is sugar
 				if(RandomUtils.coinFlip(sugarProbability))
@@ -40,6 +43,7 @@ public class Graph{
 					nodes[i] = new Node();
 			}
 		}
+		
 		//Creating the edges
 		ArrayList<Edge> tempEdge = new ArrayList<>();
 		
@@ -54,17 +58,6 @@ public class Graph{
 				tempEdge.add(new Edge(nodes[i], nodes[i+1]));
 		}
 		this.edges = tempEdge.toArray(new Edge[0]);
-		
-		for(int i = 0; i < nodes.length; i++){
-			System.out.print(nodes[i] + " ");
-			if(i%width == width-1)
-				System.out.println();
-		}
-		System.out.println();
-		System.out.println();
-		for(int i = 0; i < edges.length; i++)
-			System.out.println(edges[i].source() + " " + edges[i].target());
-		System.out.println("TEST GRID");
 	}
 	
 	/*
@@ -76,13 +69,15 @@ public class Graph{
 			File file = new File(filename);
 			Scanner scanner = new Scanner(file);
 			
-			//Initializing nodes
 			this.nodes = new Node[scanner.nextInt()];
+			
+			//Initalizing colonies
 			for(int i = 0; i < homes.length; i++){
 				System.out.println(i);
 				nodes[scanner.nextInt()-1] = homes[i];
 			}
 			
+			//Initalizing nodes
 			for(int i = 0; i < nodes.length; i++){
 				if(nodes[i] == null){
 					//Initalizes the rest of the nodes and decides if there is sugar
@@ -100,7 +95,7 @@ public class Graph{
 				tempEdge.add(new Edge(nodes[scanner.nextInt()-1],nodes[scanner.nextInt()-1]));
 			
 			this.edges = tempEdge.toArray(new Edge[0]);
-			}
+		}
 		catch(FileNotFoundException e){
 			System.out.println("The file does not exist");
 			System.exit(0);
@@ -109,34 +104,34 @@ public class Graph{
 	
 	//Contract methods
 	
+	//Returns the pheromone level between source and target.
 	public int pheromoneLevel(Node source, Node target){
 		return edgeFinder(source, target).pheromones();
 	}
 	
+	//Raises the pheromone level between source and target.
 	public void raisePheromones(Node source, Node target, int amount){
 		edgeFinder(source, target).raisePheromones(amount);
 	}
 	
+	//Returns all adjacent nodes to node
 	public Node[] adjacentTo(Node node){
 		ArrayList<Node> tempNode = new ArrayList<>();
 		
 		for(int i = 0; i < edges.length; i++){
+			//valid node if edge between either source or target and this node.
 			if(edges[i].source() == node)
 				tempNode.add(edges[i].target());
 			else if(edges[i].target() == node)
 				tempNode.add(edges[i].source());
 		}
 		
-		System.out.println("TEST ADJACENTTO");
 		Node[] nodes = tempNode.toArray(new Node[0]);
-		
-		for(int i = 0; i < nodes.length; i++){
-			System.out.println(nodes[i]);
-		}
 				
 		return nodes;
 	}
 	
+	//Decreases pheromones in all edges and maybe spawns sugar in node.
 	public void tick(){
 		//Decreases pheromones in all edges that have some
 		for(int i = 0; i < edges.length; i++){
@@ -152,15 +147,17 @@ public class Graph{
 			//Increases the amount of sugar in the chosen Node
 			nodes[chosenNode].setSugar(nodes[chosenNode].sugar() + RandomUtils.randomPoisson(avgSugar));
 		}
-		
-		System.out.println("TEST TICK");
 	}
 	
 	//Auxillary methods
+	
+	//Returns the edge between source and target.
 	private Edge edgeFinder(Node source, Node target){
 		Edge edge = null;
 		int i = 0;
 		boolean found = false;
+		
+		//Loops until an edge has been found between source and target, checks both ways
 		while(!found && i < edges.length){
 			if(edges[i].source() == source || edges[i].target() == source)
 				if(edges[i].source() == target || edges[i].target() == target){
@@ -169,9 +166,6 @@ public class Graph{
 				}
 			i++;
 		}
-		
-		System.out.println("TEST EDGEFINDER");
-		System.out.println(edge != null);
 				
 		return edge;
 	}	
